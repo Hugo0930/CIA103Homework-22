08a2c4ece537a1b171773bc92269abe200df2e79
Hugo <liupeter930@gmail.com>
2024-08-22 16:23:44 +0800

    first commit

---
+++ src/hw2/TestForNumber.java
@@ -0,0 +1,16 @@
+package hw2;
+
+public class TestForNumber {
+	public static void main(String[] args) {
+
+		for (int count = 0; count <= 100; count += 4) {
+			System.out.println(count);
+//		
+//			System.out.print("\n"); 換行符號
+//			for (int count = 0; count <= 100; count++) {
+//			if (count % 4 == 0)
+//				System.out.println(count);
+		}
+
+	}
+}
